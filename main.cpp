f41c9e56be3fc930f1b6c099c5fa19c00b7a1f5c
ulcern <ulacerniauskaite3@gmail.com>
2025-09-18 19:22:42 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,61 +1,26 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <iomanip>
-
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-using std::vector;
-using std::setw;
-using std::left;
-using std::right;
-using std::setprecision;
-
-struct Studentas{
-    string vardas;
-    string pavarde;
-    vector<int>pazymys;
-    int egzaminas;
-    double galutinis;
-};
-
-Studentas Stud_kiek(){
-    Studentas Pirmas;
-    int n, laik_paz, sum=0;
-    cout<<"Ivesk studento duomenis"<<endl;
-    cout<<"Vardas: ";
-    cin>>Pirmas.vardas;
-    cout<<"Pavarde: ";
-    cin>>Pirmas.pavarde;
-    cout<<"Kiek pazymiu turi studentas: "<<Pirmas.vardas<<" "<<Pirmas.pavarde<<"? ";
-    cin>>n;
-    for(int a=0; a<n; ++a){
-        cout<<a+1<<": ";
-        cin>>laik_paz;
-        Pirmas.pazymys.push_back(laik_paz);
-        sum+=laik_paz;
-    }
-    cout<<"Galutinis egzamino pazymys: ";
-    cin>>Pirmas.egzaminas;
-    Pirmas.galutinis=(double)sum/(double)n*0.4+Pirmas.egzaminas*0.6;
-    return Pirmas;
-}
-
 int main(){
     cout<<"Studento pazymiai"<<endl;
     int kiek;
     cout<<"Kiek studentu grupeje? ";
     cin>>kiek;
-    vector<Studentas>grupe;
+    
+    vector<Studentas> grupe;
     grupe.reserve(kiek);
-    for(int i=0; i<kiek; ++i)grupe.push_back(Stud_kiek());
+    
+    for(int i=0; i<kiek; ++i) 
+        grupe.push_back(Stud_kiek());
+    
     cout<<endl<<"Rezultatai:"<<endl;
     cout<<"----------------------------------------"<<endl;
-    cout<<setw(10)<<left<<"Pavarde"<<setw(10)<<left<<"Vardas"<<setw(10)<<left<<"Galutinis(vid.)"<<endl;
+    cout<<setw(10)<<left<<"Pavarde" 
+        <<setw(10)<<left<<"Vardas" 
+        <<setw(15)<<left<<"Galutinis(vid.)"<<endl;
     cout<<"----------------------------------------"<<endl;
-    for(const auto& s:grupe)
-        cout<<setw(10)<<left<<s.pavarde<<setw(10)<<left<<s.vardas<<setw(10)<<left<<setprecision(2)<<s.galutinis<<endl;
+    
+    for(const auto& s : grupe)
+        cout<<setw(10)<<left<<s.pavarde 
+            <<setw(10)<<left<<s.vardas 
+            <<setw(15)<<left<<std::fixed<<setprecision(2)<<s.galutinis<<endl;
+    
     return 0;
 }
