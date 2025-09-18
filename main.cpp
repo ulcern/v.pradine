e91630be48c7e361d9928f383d6db54ee36927da
ulcern <ulacerniauskaite3@gmail.com>
2025-09-18 17:40:13 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -35,8 +35,31 @@ int main (){
     cin>>Pirmas.pavarde;
     cout<<"Kiek pazymiu turi studentas: "<<Pirmas.vardas<<" "<<Pirmas.pavarde<<"? ";
     cin>>n;
+    
+        for (int a=0; a<n;a++){
+        cout<<a+1<<": ";
+        cin>>laik_paz;
+        Pirmas.pazymys.push_back(laik_paz);
+        sum+=laik_paz;
+    }
 
-    for 
+    cout<<"Galutinis egzamino pazymys: ";
+    cin>>Pirmas.egzaminas;
+
+    Pirmas.galutinis=(double)sum/(double)n*0.4+Pirmas.egzaminas*0.6;
+
+    //Parodom rezultata/sukuriam lentele
+    cout<<endl<<"Rezultatai:"<<endl;
+    cout<<"----------------------------------------"<<endl;
+    cout<<setw(10)<<left<<"Pavarde"
+        <<setw(10)<<left<<"Vardas"
+        <<setw(10)<<left<<"Galutinis (vid.)"<<endl;
+    cout<<"----------------------------------------"<<endl;
+    cout<<setw(10)<<left<<Pirmas.pavarde
+        <<setw(10)<<left<<Pirmas.vardas
+        <<setw(10)<<left<<fixed<<setprecision(2)<<Pirmas.galutinis<<endl;
+    return 0;
 }
+
 
 
