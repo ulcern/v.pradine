ee3df83afef8add25ac8dfacbe0461288ddc8d85
ulcern <ulacerniauskaite3@gmail.com>
2025-09-18 18:49:57 +0300

    main.cpp

--- main.cpp
+++ main.cpp
@@ -14,52 +14,53 @@ using std::right;
 using std::fixed;
 using std::setprecision;
 
-struct Studentas {
+struct Studentas{
     string vardas;
     string pavarde;
-    vector<int> pazymys;
+    vector<int>pazymys;
     int egzaminas;
     double galutinis;
 };
 
-int main (){
-    int n,laik_paz, sum=0;
-    cout<<"Studentu pazymiu programa"<<endl;
-
+Studentas Stud_kiek(){
     Studentas Pirmas;
-
+    int n, laik_paz, sum=0;
     cout<<"Ivesk studento duomenis"<<endl;
     cout<<"Vardas: ";
     cin>>Pirmas.vardas;
+
     cout<<"Pavarde: ";
     cin>>Pirmas.pavarde;
+
     cout<<"Kiek pazymiu turi studentas: "<<Pirmas.vardas<<" "<<Pirmas.pavarde<<"? ";
     cin>>n;
-    
-        for (int a=0; a<n;a++){
+
+    for(int a=0; a<n; ++a) {
         cout<<a+1<<": ";
         cin>>laik_paz;
         Pirmas.pazymys.push_back(laik_paz);
         sum+=laik_paz;
     }
-
     cout<<"Galutinis egzamino pazymys: ";
     cin>>Pirmas.egzaminas;
 
     Pirmas.galutinis=(double)sum/(double)n*0.4+Pirmas.egzaminas*0.6;
-
-    //Parodom rezultata/sukuriam lentele
-    cout<<endl<<"Rezultatai:"<<endl;
-    cout<<"----------------------------------------"<<endl;
-    cout<<setw(10)<<left<<"Pavarde"
-        <<setw(10)<<left<<"Vardas"
-        <<setw(10)<<left<<"Galutinis (vid.)"<<endl;
-    cout<<"----------------------------------------"<<endl;
-    cout<<setw(10)<<left<<Pirmas.pavarde
-        <<setw(10)<<left<<Pirmas.vardas
-        <<setw(10)<<left<<fixed<<setprecision(2)<<Pirmas.galutinis<<endl;
-    return 0;
+    return Pirmas;
 }
 
-
-
+int main(){
+    cout<<"Studento pazymiai"<<endl;
+    int kiek;
+    cout<<"Kiek studentu grupeje? ";
+    cin>>kiek;
+    vector<Studentas>grupe;
+    grupe.reserve(kiek);
+    for(int i=0; i<kiek; ++i)grupe.push_back(Stud_kiek());
+    cout<<endl<<"Rezultatai:"<<endl;
+    cout<<"----------------------------------------"<<endl;
+    cout<<setw(10)<<left<<"Pavarde"<<setw(10)<<left<<"Vardas"<<setw(10)<<left<<"Galutinis (vid.)"<<endl;
+    cout<<"----------------------------------------"<<endl;
+    for(const auto&s:grupe)
+        cout<<setw(10)<<left<<s.pavarde<<setw(10)<<left<<s.vardas<<setw(10)<<left<<fixed<<setprecision(2)<<s.galutinis<<endl;
+    return 0;
+}
