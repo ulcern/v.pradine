c5a5950c03632ea3bdaa7d1111b9a971636c3734
ulcern <ulacerniauskaite3@gmail.com>
2025-09-18 17:01:40 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,39 @@
 #include <iostream>
-#include<iomanip>
-#include<vector>
-#include<string>
-using namespace std;
+#include <string>
+#include <vector>
+#include <iomanip>
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::vector;
+using std::setw;
+using std::left;
+using std::right;
+using std::fixed;
+using std::setprecision;
+
+struct Studentas {
+    string vardas;
+    string pavarde;
+    vector<int> pazymys;
+    int egzaminas;
+    double galutinis;
+};
+
+int main (){
+    int n,laik_paz, sum=0;
+    cout<<"Studentu pazymiu programa"<<endl;
+
+    Studentas Pirmas;
+
+    cout<<"Ivesk studento duomenis"<<endl;
+    cout<<"Vardas: ";
+    cin>>Pirmas.vardas;
+    cout<<"Pavarde: ";
+    cin>>Pirmas.pavarde;
+    cout<<"Kiek pazymiu turi studentas: "<<Pirmas.vardas<<" "<<Pirmas.pavarde<<"? ";
+    cin>>n;
 }
 
